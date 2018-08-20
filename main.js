8cfbe7c66cbc6d379d4ab664d5b9a64f15378bbb
Howie Zhu <hwie.zhu@gmail.com>
2018-08-19 21:56:13 -0400

    shorten the line of codes to include directionality as a array and randomized choosing the location. Created limiters for the submarine that can travel to.

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
+const randomDirection = Math.random();
+const UP = -6;
+const DOWN = 6;
+const LEFT = -1;
+const RIGHT = 1;
+const direction = [UP, RIGHT, DOWN, LEFT];
+const leftDownRight = [LEFT, DOWN, RIGHT];
+const leftUpRight = [LEFT, UP, RIGHT];
+const rightDownUp = [RIGHT, DOWN, UP];
+const leftDownUp = [LEFT, DOWN, UP];
+const rightDown = [RIGHT, DOWN];
+const leftDown = [LEFT, DOWN];
+const rightUp = [RIGHT, UP];
+const leftUp = [LEFT, UP];
 let submarine = 0;
 const gameboard = document.querySelector('.gameboard');
 
@@ -30,6 +44,16 @@ function move() {
   } else if (submarine % 6 === 5 && submarine !== 5 && submarine !== 35) {
     submarine += leftDownUp[chooseDirectionIndex(leftDownUp.length)];
   } else if (submarine === 0) {
+    submarine += rightDown[chooseDirectionIndex(rightDown.length)];
+  } else if (submarine === 5) {
+    submarine += leftDown[chooseDirectionIndex(leftDown.length)];
+  } else if (submarine === 30) {
+    submarine += rightUp[chooseDirectionIndex(rightUp.length)];
+  } else if (submarine === 35) {
+    submarine += leftUp[chooseDirectionIndex(leftUp.length)];
+  } else {
+    submarine += direction[chooseDirectionIndex(direction.length)];
+  }
 
 const guessFeedbackElement = document.querySelector('.guess-feedback');
 gameboard.addEventListener('click', function (event) {
